eff7d758a0aaaf3a67ca660eaef8771842a7a755
Grimor Fildrance <Fildrance@gmail.com>
2020-10-23 16:37:36 +0300

    #feat: request-logging middleware added

--- src/Kaizen.ApiGateway/Startup.cs
+++ src/Kaizen.ApiGateway/Startup.cs
@@ -18,6 +18,8 @@ namespace Kaizen.ApiGateway
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+			app.UseMiddleware<RequestLoggingMiddleware>();
+			
             app.UseMiddleware<ValidationExceptionPackagingMiddleware>();
 
             //app.UseHttpsRedirection();
